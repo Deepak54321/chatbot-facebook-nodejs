4174315dd81afdbc805429983773d866dd8960d7
Deepak54321 <31242754+Deepak54321@users.noreply.github.com>
2017-11-10 10:45:26 -0800

    Update app.js

--- app.js
+++ app.js
@@ -275,6 +275,28 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
                 sendTextMessage(sender, responseText);
             }
                 break;
+				 case "testuser-data":
+            if(isDefined(contexts[0]) && contexts[0].name=='welcomeyamaha' && contexts[0].parameters) {
+                let phone_number = (isDefined(contexts[0].parameters['TNumber']) &&
+                    contexts[0].parameters['TNumber'] != '') ? contexts[0].parameters['TNumber'] : '';
+                let email = (isDefined(contexts[0].parameters['TEmail']) &&
+                    contexts[0].parameters['TEmail'] != '') ? contexts[0].parameters['TEmail'] : '';
+                let testdrive_customer_interest = (isDefined(contexts[0].parameters['TestRideCustomerInterest']) &&
+                    contexts[0].parameters['TestRideCustomerInterest'] != '') ? contexts[0].parameters['TestRideCustomerInterest'] : '';
+                let testride_Feedback = (isDefined(contexts[0].parameters['TestRideFeedback']) &&
+                    contexts[0].parameters['TestRideFeedback'] != '') ? contexts[0].parameters['TestRideFeedback'] : '';
+                let pincode=(isDefined(contexts[0].parameters['TPincode']) &&
+                    contexts[0].parameters['TPincode'] != '') ? contexts[0].parameters['TPincode'] : '';
+                
+                if (phone_number != '' && email != '') {
+                    let emailContent =  'Phone Number:=' + phone_number + 'email:=' + email + 'customer' +
+                        'Customer Interest' + testdrive_customer_interest + 'Product_Feedback '+ testride_Feedback +'Pincode'+pincode+'';
+                    sendTextMessage(sender, emailContent);
+                    //responseText=emailContent;
+                }
+                sendTextMessage(sender, responseText);
+            }
+                break;
         case "dealer-price":
             var request = require('request');
             request({
